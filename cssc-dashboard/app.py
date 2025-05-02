86c37fb7c4ac73205fb70b11c4813816c66125ae
sam <samscott640@gmail.com>
2025-05-02 13:32:26 +0800

    Added functionality to change scrolling text. Changed login appearance.

--- cssc-dashboard/app.py
+++ cssc-dashboard/app.py
@@ -17,33 +17,23 @@ app.secret_key = os.getenv('SECRET_KEY')
 
 password = os.getenv('PASSWORD')
 
+# Base file path
+base = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+print()
+
 
 with app.open_resource('static/events.json') as f:
     data = json.load(f)
 
-def load_door_stat():
-    root = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
-    with open(f'{root}static/door_status.json', 'r') as f:
-        door_stat = json.load(f)
-
-        return door_stat
+def load_door_status():
+    with open(f'{base}static/door_status.json', 'r') as f:
+        _data = json.load(f)
+        return _data
 
 
-def write_door_stat(status):
-
-    root = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
-    with (open(f'{root}/static/door_status.json', 'w') as f):
-        json.dump({"door_open": status}, f)
-
-
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    _pass = request.form.get('password')
-
-    if _pass == password:
-        session['logged_in'] = True
-        return flask.redirect(flask.url_for('dashboard'))
-    return flask.redirect(flask.url_for('login'))
+def write_door_status(_data):
+    with open(f'{base}/static/door_status.json', 'w') as f:
+        json.dump(_data, f)
 
 @app.route('/api/auth', methods=['POST'])
 def door_auth():
@@ -59,18 +49,18 @@ def dashboard():
     if not session.get('logged_in'):
         return flask.redirect(flask.url_for('login'))
 
-    door_status = load_door_stat()
+    door_status = load_door_status()
     if door_status["door_open"]:
         door_stat = "Open"
     else:
         door_stat = "Closed"
-    return flask.render_template('dashboard.html', door_status=door_stat)
+
+    return flask.render_template('dashboard.html', door_status=door_stat, scrolltext=door_status["scrolltext"])
 
 @app.route('/api/get_events')
 def get_events():
     if not session.get('logged_in'):
         return flask.redirect(flask.url_for('login'))
-
 
     return flask.jsonify(data)
 
@@ -89,14 +79,16 @@ def get_door_status():
     if not session.get('logged_in'):
         return flask.redirect(flask.url_for('login'))
 
-    return flask.jsonify(load_door_stat())
+    return flask.jsonify(load_door_status())
 
 @app.route('/api/open_door')
 def open_door():
     if not session.get('logged_in'):
         return flask.redirect(flask.url_for('login'))
 
-    write_door_stat(1)
+    _data = load_door_status()
+    _data['door_open'] = 1
+    write_door_status(_data)
     return flask.redirect(flask.url_for('dashboard'))
 
 @app.route('/api/close_door')
@@ -104,5 +96,20 @@ def close_door():
     if not session.get('logged_in'):
         return flask.redirect(flask.url_for('login'))
 
-    write_door_stat(0)
+    _data = load_door_status()
+    _data['door_open'] = 0
+    write_door_status(_data)
+    return flask.redirect(flask.url_for('dashboard'))
+
+@app.route('/api/scrolltext_update', methods=['POST'])
+def scrolltext_update():
+    if not session.get('logged_in'):
+        return flask.redirect(flask.url_for('login'))
+
+    new_scrolltext = request.form.get('scrolltext')
+    print(new_scrolltext)
+    _data = load_door_status()
+    _data["scrolltext"] = new_scrolltext
+    write_door_status(_data)
+
     return flask.redirect(flask.url_for('dashboard'))
---
+++ cssc-dashboard/static/js/login.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('login_form');
+success = document.getElementById('password_success');
+
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent the default form submission
+
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch('/api/auth', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      // Handle success (e.g., display a message)
+        response.json().then((data) => {
+            if (data["logged_in"]) {
+                success.innerHTML = "<b><i>Logged in. Redirecting...</i></b>";
+                window.location.replace("/dashboard");
+            } else {
+                success.innerHTML = "Error: not logged in";
+            }
+
+        })
+
+    } else {
+      // Handle error
+      console.error('Form submission failed');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
